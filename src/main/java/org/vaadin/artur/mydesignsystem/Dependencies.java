75619d699e5a923f8432ffc31b7f9cec7c9ccbe1
Artur Signell <artur@vaadin.com>
2020-09-24 12:54:41 +0300

    Include FontAwesome and make FA SVGs available in /fa/svg in the app project

---
+++ src/main/java/org/vaadin/artur/mydesignsystem/Dependencies.java
@@ -0,0 +1,8 @@
+package org.vaadin.artur.mydesignsystem;
+
+import com.vaadin.flow.component.dependency.NpmPackage;
+
+@NpmPackage(value = "@fortawesome/fontawesome-free", version = "5.14.0")
+public class Dependencies {
+
+}
